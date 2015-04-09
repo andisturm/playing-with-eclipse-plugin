276da16eed395d204d71936ef5592e5c435273c4
andisturm <andi.sturm@gmx.de>
2015-04-09 22:55:07 +0200

    Added one comment (Author)

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -3,6 +3,7 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		// Author: Andreas Sturm
 		
 		System.out.println("Hello World, to all GitHub friends!");
 	}
